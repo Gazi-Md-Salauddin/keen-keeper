9f59fd37c157f9cf826945a4e515b949b930bf67
Gazi-Md-Salauddin <mdsalauddin329132@gmail.com>
2026-04-13 23:42:12 +0600

    Designed Banner with responsive

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className="text-center pt-16 space-y-4">
+      <h1 className="text-2xl font-bold">Friends to keep close in your life</h1>
+      <p className="text-[#64748B]">Your personal shelf of meaningful connections. Browse, tend, and nurture the
+relationships that matter most.</p>
+<button className="bg-green-800 text-white rounded-xl p-2">+ Add a Friend</button>
+    </div>
+  )
+}
+
+export default Banner
