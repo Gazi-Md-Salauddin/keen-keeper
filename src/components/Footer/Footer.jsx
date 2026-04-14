2910c6f163bb6c677b250e8a598ae208ccef7979
Gazi-Md-Salauddin <mdsalauddin329132@gmail.com>
2026-04-14 11:17:59 +0600

    Designed footer with fully responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,40 @@
-import React from 'react'
-import Footerlogo from '../../assets/logo-xl.png'
+import React from "react";
+import Footerlogo from "../../assets/logo-xl.png";
+import Instagram from "../../assets/instagram.png";
+import Facebook from "../../assets/facebook.png";
+import Twitter from "../../assets/twitter.png";
 
 const Footer = () => {
-  return (
-    <div>
-      <div className="bg-[#235242] text-white text-center">
-        <div className="flex justify-center">
-          <img src={Footerlogo} alt="Footer Logo"/>
-        </div>
-        <p>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
-        <h2>Social Links</h2>
+    return (
         <div>
-          
-        </div>
-      </div>
-    </div>
-  )
-}
+            <div className="bg-[#235242] text-white text-center px-4 mt-6 md:pt-10 px-6 space-y-4">
+                <div className="flex justify-center pt-6">
+                    <img src={Footerlogo} alt="Footer Logo" />
+                </div>
+                <p>
+                    Your personal shelf of meaningful connections. Browse, tend,
+                    and nurture the relationships that matter most.
+                </p>
+                <h2 className="text-md font-bold">Social Links</h2>
+                <div className="flex justify-center gap-2">
+                    <img src={Instagram} />
+                    <img src={Facebook} />
+                    <img src={Twitter} />
+                </div>
 
-export default Footer
+                <div className="flex-1 md:flex justify-between mt-4 pb-4">
+                    <div>
+                        <p>&copy;2026 keenkeeper. All rights reserved. </p>
+                    </div>
+                    <div className="flex gap-3">
+                        <p>Privacy Policy</p>
+                        <p>Terms of Service</p>
+                        <p>Cookies</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Friends = () => {
-  return (
-    <div className="px-47 mt-10">
-      <h2 className="text-xl font-bold">Your Friends</h2>
-    </div>
-  )
-}
+    return (
+        <div className="px-4 md:px-47 mt-10">
+            <h2 className="text-xl font-bold">Your Friends</h2>
 
-export default Friends
+            <div>
+                <div className="card bg-base-100 w-96 shadow-sm">
+                    <figure className="px-10 pt-10">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes"
+                            className="rounded-xl"
+                        />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Card Title</h2>
+                        <p>
+                            A card component has a figure, a body part, and
+                            inside body there are title and actions parts
+                        </p>
+                        <div className="badge badge-secondary">NEW</div>
+                        <div className="badge badge-secondary">NEW</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Friends;
